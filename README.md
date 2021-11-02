ef913579bb17a0f55fca4abf891815884291ce6b
ixnp <enprocopios@gmail.com>
2021-11-02 09:41:10 -0700

    01-intro-to-react-solution;

---
+++ README.md
@@ -0,0 +1,5 @@
+# Phase-2-Lecture-Activity-Flatiron-Bakes-092021
+The solution code for every lecture will be on branches with main having the most up-to-date code.
+
+
+
---
+++ flatiron-bakes/src/components/CakeCard.js
@@ -0,0 +1,9 @@
+function CakeCard(props){
+    return(
+        <>
+            <h1>Flavor: {props.flavor}</h1>
+            <p>Price: {props.price}</p>
+        </>
+    )
+}
+export default CakeCard
